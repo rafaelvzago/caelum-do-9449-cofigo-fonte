5406cc76550b5f3eadc75972090c7dcb4ab6cb8b
Rafael Zago <rafaelvzago@gmail.com>
2020-11-10 02:50:19 -0300

    First commit

---
+++ src/br/com/alura/forum/config/DataSourceConfiguration.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+	@Bean
+	@Profile("producao")
+	public DataSource producao() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/alura_forum?useSSL=false&serverTimezone=UTC");
+		dataSource.setUsername("alura");
+		dataSource.setPassword("qwerty123");
+		return dataSource;
+	}
+	
+	@Bean
+	@Profile("desenvolvimento")
+	public DataSource desenvolvimento() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/alura_forum?useSSL=false&serverTimezone=UTC");
+		dataSource.setUsername("alura");
+		dataSource.setPassword("qwerty123");
+		return dataSource;
+	}
+	
+}
